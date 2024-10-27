af48b8ecfde86292b9682d9604380713cdcf9f1b
Eduard Gumen <hydrospirt@mail.ru>
2024-10-27 21:59:36 +0300

    Add docstringJS and updater comments

--- unipass/static/js/clip.js
+++ unipass/static/js/clip.js
@@ -1,11 +1,30 @@
-function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(function() {
+/**
+ * Copies the given text to the user's clipboard using the `copyToClipboard`
+ * function from the `clip.js` script.
+ *
+ * @param {string} elementId - The id of the input element to copy from
+ */
+function copyToClipboard(elementId) {
+    let inputElement;
+    if (elementId === 'password') {
+        inputElement = document.getElementById('password');
+    } else {
+        inputElement = document.querySelector(`input[value='${elementId}']`);
+    }
+    navigator.clipboard.writeText(inputElement.value).then(function() {
         showNotification("Пароль скопирован в буфер обмена!");
     }).catch(function(err) {
         showNotification("Не удалось скопировать пароль: " + err);
     });
 }
 
+/**
+ * Displays a notification with the specified message at the top right corner of the page.
+ * The notification includes a delete button to manually close it and automatically
+ * disappears after 5 seconds.
+ *
+ * @param {string} message - The message to display within the notification.
+ */
 function showNotification(message) {
     const notification = document.createElement("div");
     notification.className = "notification is-success is-light";
@@ -17,13 +36,13 @@ function showNotification(message) {
     const notificationContainer = document.getElementById("notification-container");
     notificationContainer.appendChild(notification);
 
-    // Добавляем обработчик для закрытия оповещения
+    // Adding a handler to close the alert
     const deleteButton = notification.querySelector(".delete");
     deleteButton.addEventListener("click", () => {
         notification.parentNode.removeChild(notification);
     });
 
-    // Удаляем оповещение через 5 секунд
+    // We delete the notification after 5 seconds
     setTimeout(() => {
         if (notification.parentNode) {
             notification.parentNode.removeChild(notification);
