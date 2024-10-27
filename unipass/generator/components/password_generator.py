d6f07bae7a6458ad9f75d3e299f3bf97bfcc3036
Eduard Gumen <hydrospirt@mail.ru>
2024-10-27 21:56:28 +0300

    Add docstrings and change random to secrets module

--- unipass/generator/components/password_generator.py
+++ unipass/generator/components/password_generator.py
@@ -1,4 +1,4 @@
-import random
+import secrets
 import string
 from django_unicorn.components import UnicornView
 
@@ -11,16 +11,33 @@ class PasswordGeneratorView(UnicornView):
     include_special_chars: bool = True
 
     def generate_password(self):
+        """
+        Generates the main password and password options depending on
+        the selected options (length, inclusion of numbers,
+        inclusion of special characters).
+        """
         characters = string.ascii_letters
         if self.include_digits:
             characters += string.digits
         if self.include_special_chars:
             characters += string.punctuation
 
-        self.password = "".join(random.choice(
-            characters) for _ in range(self.length))
-        self.password_options = ["".join(random.choice(
-            characters) for _ in range(self.length)) for _ in range(10)]
+        # Generating the main password
+        self.password = "".join(
+            secrets.choice(characters) for _ in range(self.length))
+
+        # Generating password options
+        self.password_options = [
+            "".join(secrets.choice(
+                characters) for _ in range(
+                    self.length)) for _ in range(10)
+        ]
 
     def copy_to_clipboard(self, text):
+        """
+        Copies the given text to the user's clipboard using the
+        `copyToClipboard` function from the `clip.js` script.
+
+        :param text: The text to copy to the user's clipboard
+        """
         self.call("copyToClipboard", text)
