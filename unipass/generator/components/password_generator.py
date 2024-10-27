202c4b37d0f39ff951bede4e03722d62b23bc652
Eduard Gumen <hydrospirt@mail.ru>
2024-10-27 22:10:18 +0300

    Update generate_password, add min and max length

--- unipass/generator/components/password_generator.py
+++ unipass/generator/components/password_generator.py
@@ -10,34 +10,44 @@ class PasswordGeneratorView(UnicornView):
     include_digits: bool = True
     include_special_chars: bool = True
 
-    def generate_password(self):
+    def generate_password(self) -> None:
         """
-        Generates the main password and password options depending on
-        the selected options (length, inclusion of numbers,
-        inclusion of special characters).
+        Generates the main password and a list of password options based on
+        the selected criteria: length, inclusion of digits,
+        and special characters.
+
+        :param self: The PasswordGeneratorView instance
+        :type self: PasswordGeneratorView
+
+        The generated passwords are stored in the `password` and
+        `password_options` attributes.
         """
-        characters = string.ascii_letters
+        characters: str = string.ascii_letters
         if self.include_digits:
             characters += string.digits
         if self.include_special_chars:
             characters += string.punctuation
 
-        # Generating the main password
-        self.password = "".join(
+        self.length = min(self.length, 50)
+        self.length = max(self.length, 8)
+
+        # Generate the main password
+        self.password: str = "".join(
             secrets.choice(characters) for _ in range(self.length))
 
-        # Generating password options
-        self.password_options = [
-            "".join(secrets.choice(
-                characters) for _ in range(
-                    self.length)) for _ in range(10)
+        # Generate a list of password options
+        self.password_options: list[str] = [
+            "".join(secrets.choice(characters) for _ in range(self.length))
+            for _ in range(10)
         ]
 
-    def copy_to_clipboard(self, text):
+    def copy_to_clipboard(self, text: str) -> None:
         """
         Copies the given text to the user's clipboard using the
         `copyToClipboard` function from the `clip.js` script.
 
         :param text: The text to copy to the user's clipboard
         """
+        # Call the `copyToClipboard` function from the `clip.js` script
+        # with the given text as the argument
         self.call("copyToClipboard", text)
